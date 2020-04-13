9ecea11e54ed02ccd0c4956dc9ea872df0325ebc
safeoff <sgmrrc@gmail.com>
2020-04-13 21:21:57 +0900

    oj

--- temp.cpp
+++ temp.cpp
@@ -1,34 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i=0; i<(int)n; i++)
-#define REPR(i, n) for(int i=(int)n-1; i>=0; i--)
-#define FOR(i, m, n) for(int i=(int)m; i<(int)n; i++)
-#define FORR(i, m, n) for(int i=(int)m-1; i>=(int)n; i--)
+typedef long long ll;
+#define REP(i, n) for(ll i=0; i<(ll)n; i++)
+#define REPR(i, n) for(ll i=(ll)n-1; i>=0; i--)
+#define FOR(i, m, n) for(ll i=(ll)m; i<(ll)n; i++)
+#define FORR(i, m, n) for(ll i=(ll)m-1; i>=(ll)n; i--)
 #define ALL(x) (x).begin(), (x).end()
 #define ASC(v) sort(ALL(v));
 #define DESC(v) sort(ALL(v), greater<int>());
 #define UNIQ(a) sort(ALL(a));(a).resize(unique(ALL(a)) - a.begin());
+#define PRINTD(x, d) cout << fixed << setprecision(d) << x << "\n";
 #define DEBUG(x) cout<<#x<<": "<<x<<"\n"
-typedef long long ll;
 const double PI = acos(-1);
 ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
 bool isprime(int x){ int i; if(x<2)return 0; else if(x==2) return 1; if(x%2==0) return 0; for(i=3; i*i<=x; i+=2) if(x%i==0) return 0; return 1; }
 int digsum(int n) { int r=0; while(n>0) { r+=n%10; n/=10; } return r; }
-struct nya{ nya(){ cin.tie(0); ios::sync_with_stdio(false); } }nyan;
-
-int main() {
-	DEBUG(lcm(20000,41));
-	//string S;
-	//cin >> S;
-	//ll N;
-	//cin >> N;
-	//vector<ll> A(N);
-	//REP(i, N) cin >> A[i];
-
-	//ll res = 0;
-	//cout << res << "\n";
-
-	//bool can = true;
-	//if (can) cout << "Yes" << "\n";
-	//else cout << "No" << "\n";
+template<class T> bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T> bool chmin(T &a, const T &b) { if (a>b) { a = b; return 1; } return 0; }
+void rcmb(ll *x,ll s,ll r,function<void(ll *)>f){if(r==0){f(x);}else{if(s<0)return;rcmb(x,s-1,r,f);x[r-1]=s;rcmb(x,s-1,r-1,f);}}
+void cmb(ll n,ll k,function<void(ll *)>f){ll x[k];rcmb(x,n-1,k,f);}
+void prm(ll n,function<void(ll *)>f){ll x[n];for(ll i=0;i<n;i++)x[i]=i;do{f(x);}while(next_permutation(x,x+n));}
+void prm(ll n,ll k,function<void(ll *)>f){cmb(n,k,[&](ll *c){prm(k,[&](ll *p){ll x[k];for(ll i=0;i<k;i++){x[i]=c[p[i]];}f(x);});});}
+// prm(4, 2, [](ll *x){ cout << x[0] << "," << x[1] << "\n"; });
+ 
+int main(){
 }
